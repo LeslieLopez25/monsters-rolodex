cc0675270d3e095453f8661ef8c49722d565b065
Leslie Lopez <leslie2515lopez@gmail.com>
2022-12-22 22:16:20 -0600

    Finished setting up searchbox component typescript

--- src/components/search-box/search-box.component.tsx
+++ src/components/search-box/search-box.component.tsx
@@ -1,6 +1,18 @@
+import { ChangeEventHandler } from "react";
+
 import "./search-box.styles.css";
 
-const SearchBox = ({ className, placeholder, onChangeHandler }) => (
+type SearchBoxProps = {
+  className: string;
+  placeholder?: string;
+  onChangeHandler: ChangeEventHandler<HTMLInputElement>;
+};
+
+const SearchBox = ({
+  className,
+  placeholder,
+  onChangeHandler,
+}: SearchBoxProps) => (
   <input
     className={`search-box ${className}`}
     type="search"
